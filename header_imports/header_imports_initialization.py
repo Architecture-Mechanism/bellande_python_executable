99cae85ad47fee3d83321fedf2aaabc0c3f668cc
RonaldsonBellande <ronaldsonbellande@gmail.com>
2025-07-24 21:56:56 -0400

    latest pushes

--- header_imports/header_imports_initialization.py
+++ header_imports/header_imports_initialization.py
@@ -1,5 +1,20 @@
-from analyzer import *
-from collector import *
-from compiler import *
-from builder import *
-from utilities import *
+# Copyright (C) 2025 Bellande Architecture Mechanism Research Innovation Center, Ronaldson Bellande
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from bellande_python_executable.analyzer import *
+from bellande_python_executable.collector import *
+from bellande_python_executable.compiler import *
+from bellande_python_executable.builder import *
+from bellande_python_executable.utilities import *
---
+++ setup.py
@@ -0,0 +1,55 @@
+# Copyright (C) 2025 Bellande Architecture Mechanism Research Innovation Center, Ronaldson Bellande
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="bellande_python_executable",
+    version="0.0.1",
+    description="Bellande python Executable is a library that makes python code into an executable",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="RonaldsonBellande",
+    author_email="ronaldsonbellande@gmail.com",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    install_requires=[
+        "numpy",
+    ],
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python",
+    ],
+    keywords=["package", "setuptools"],
+    python_requires=">=3.0",
+    extras_require={
+        "dev": ["pytest", "pytest-cov[all]", "mypy", "black"],
+    },
+    entry_points={
+        'console_scripts': [
+            'bellande_python_executable = bellande_python.bellande_python_executable:main',
+        ],
+    },
+    project_urls={
+        "Home": "https://github.com/Architecture-Mechanism/bellande_python_executable",
+        "Homepage": "https://github.com/Architecture-Mechanism/bellande_python_executable",
+        "documentation": "https://github.com/Architecture-Mechanism/bellande_python_executable",
+        "repository": "https://github.com/Architecture-Mechanism/bellande_python_executable",
+    },
+)
